65734d02cd6e50ffa0f1ad541462feca7ab36966
Haseeb Zahid <haseebzahid89@yahoo.com>
2018-03-14 00:06:43 +0500

    Delete chap1_4.py

--- chap1_4.py
+++
@@ -1,9 +0,0 @@
-languages = "Languages:\n\tPython\n\tC\n\tJavaScript"
-print(languages)
-
-name = "!  Hello World  !"
-print(name)
-print(name.rstrip('!'))
-print(name.lstrip('!'))
-name1 = 'hello world'
-print(name1.strip('h'))
